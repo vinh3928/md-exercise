6678042d5d9624c82d45bee565c1378dccc679f0
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-05-30 14:20:16 -0600

    added showdown

---
+++ app.js
@@ -0,0 +1,6 @@
+var input = document.getElementById("input"),
+  output  = document.getElementById("output"),
+converter = new showdown.Converter();
+input.addEventListener("input", function(e) {
+  output.innerHTML = converter.makeHtml(e.target.value);
+});
